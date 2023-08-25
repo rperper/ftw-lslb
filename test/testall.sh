c2bfb2d9cee3e68240cffa3d7aafcaea11970164
Bob Perper <rperper@litespeedtech.com>
2023-08-25 15:25:04 +0000

    Report elasped time on full test

--- test/testall.sh
+++ test/testall.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo 'Test all rules and write to test.out'
 rm test.out
+start=$(date +%s)
 for d in /usr/local/lslb/conf/coreruleset/tests/regression/tests/*/ ; do
        py.test -vs --tb=short ./CRS_Tests.py --config=lslb --ruledir=$d>>test.out
 done
+end=$(date +%s)
+echo "Test time: $(($end-$start)) seconds"
 
